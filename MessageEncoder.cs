f83b22f441224658db1f7df0f101358c6ab11801
six <jcuudeh@gmail.com>
2022-07-10 15:51:33 +0100

    Initial commit

---
+++ MessageEncoder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace MessageEncoder
+{
+    public static class MessageEncoder
+    {
+        public static string EncodeMessage(string message)
+        {
+            byte[] encodedBytes = Encoding.UTF8.GetBytes(message);
+            string encodedMessage = WebEncoders.Base64UrlEncode(encodedBytes);
+            return encodedMessage;
+        }
+
+        public static string DecodeMessage(string encodedMessage)
+        {
+            byte[] decodedBytes = WebEncoders.Base64UrlDecode(encodedMessage);
+            string decodedMessage = Encoding.UTF8.GetString(decodedBytes);
+            return decodedMessage;
+        }
+    }
+}
